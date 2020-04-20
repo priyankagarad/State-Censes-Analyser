1d99ac2c70a465198d1fcc0a5353cc6de76e34ed
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-20 13:13:45 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/censusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.censusanalyser;
+public class StateCensusAnalyser {
+    public static void main(String args[])
+    {
+        //Welcome Message
+        System.out.println("Welcome to State CensusAnalyser");
+    }
+}
