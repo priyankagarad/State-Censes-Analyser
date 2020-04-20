bdc502e8a72fdf1c7d7bdd0aa90c90d2e4943435
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-21 02:24:51 +0530

    ADDED:UC1 Load Indian State Census data from CSV File

---
+++ src/main/java/com/censusanalyser/CSVStateCensus.java
@@ -0,0 +1,38 @@
+package com.censusanalyser;
+import com.opencsv.bean.CsvBindByName;
+public class CSVStateCensus
+{
+    @CsvBindByName(column = "State")
+    private String State;
+    @CsvBindByName(column = "Population")
+    private String Population;
+    @CsvBindByName(column = "AreaInSqKm")
+    private String AreaInSqKm;
+    @CsvBindByName(column = "DensityPerSqKm")
+    private String DensityPerSqKm;
+
+    public String getState() {
+        return State;
+    }
+    public void setState(String state) {
+        this.State = state;
+    }
+    public String getPopulation() {
+        return Population;
+    }
+    public void setPopulation(String population) {
+        this.Population = population;
+    }
+    public String getAreaInSqKm() {
+        return AreaInSqKm;
+    }
+    public void setAreaInSqKm(String areaInSqKm) {
+        AreaInSqKm = areaInSqKm;
+    }
+    public String getDensityPerSqKm() {
+        return DensityPerSqKm;
+    }
+    public void setDensityPerSqKm(String densityPerSqKm) {
+        DensityPerSqKm = densityPerSqKm;
+    }
+}
--- src/main/java/com/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/censusanalyser/StateCensusAnalyser.java
@@ -1,8 +1,40 @@
 package com.censusanalyser;
-public class StateCensusAnalyser {
-    public static void main(String args[])
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import static java.nio.file.Files.newBufferedReader;
+
+public class StateCensusAnalyser
+{
+    public static void main(String[] args) throws IOException
     {
-        //Welcome Message
-        System.out.println("Welcome to State CensusAnalyser");
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        stateCensusAnalyser.loadData();
+    }
+    private static final String DATA_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
+    public int loadData() throws IOException
+    {
+        int totalRecords = 0;
+        try (Reader reader = newBufferedReader(Paths.get(DATA_CSV_FILE_PATH));) {
+            CsvToBean<CSVStateCensus> csvStateCensusBeanObj = new CsvToBeanBuilder(reader)
+                    .withType(CSVStateCensus.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVStateCensus> csvStateCensusIterator = csvStateCensusBeanObj.iterator();
+            while (csvStateCensusIterator.hasNext()) {
+                CSVStateCensus csvStateCensus = csvStateCensusIterator.next();
+                System.out.println("State: " + csvStateCensus.getState());
+                System.out.println("Population: " + csvStateCensus.getPopulation());
+                System.out.println("AreaInSquareKm: " + csvStateCensus.getAreaInSqKm());
+                System.out.println("DensityPerSquareKm: " + csvStateCensus.getDensityPerSqKm());
+                System.out.println("----------------------------");
+                totalRecords++;
+            }
+        }
+        return totalRecords;
     }
 }
+
